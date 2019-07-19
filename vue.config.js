16911b70aa9627dc59eeeb4b62ea339c45b40e21
Nicholas Carr <ncarr@users.noreply.github.com>
2019-07-19 01:27:25 -0400

    Ugly hack

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: './worker/sw.ts'
+      swSrc: './worker/sw.ts',
+      swDest: 'service-worker.js'
     }
-  }
+  },
+  // Quick hack to fix problem with workbox-webpack-plugin
+  productionSourceMap: false
 }
